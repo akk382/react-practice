000bceae847a59f2a1970b92ced9bdeb8318cbd7
akk382 <arunyoyo00@gmail.com>
2024-09-22 15:32:31 -0400

    Migrated script code to App.js file

---
+++ App.js
@@ -0,0 +1,18 @@
+// // Without react
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello World!";
+// const rootElement = document.getElementById('root');
+// rootElement.appendChild(heading);
+
+// With react
+const heading1 = React.createElement("h1", {
+    id: "h1tag",
+    className: "header"
+},"Heading 1"); // This is an react object with props(the 2nd param, and remaining params are children), key, type(=h1)
+const heading2 = React.createElement("h2", {}, "Heading 2");
+const innerElements = React.createElement("div", {id: "container"}, [heading1, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(innerElements); // This only uses #root element above, since we only selected the root element. It doesn't modify other tags. 
+
